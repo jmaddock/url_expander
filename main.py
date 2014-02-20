9a317365b4a38a59d0634d45af93fb77a8e83153
jmaddock <skimaddster@gmail.com>
2014-02-19 21:21:23 -0800

    first commit!

---
+++ main.py
@@ -0,0 +1,39 @@
+import urllib2
+from urlparse import urlparse
+from bs4 import BeautifulSoup
+
+URL = 'http://t.co/EzlKnfAeUi'
+URL2 = 'http://www.adfadfasdf.com'
+
+def expand(url):
+    url_data = {'url':url}
+
+    try:
+        response = urllib2.urlopen(url)
+
+    except urllib2.HTTPError as e:
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+        return e.code
+
+    except urllib2.URLError as e:
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+        return e.reason
+
+    else:
+        url_data['long-url'] = response.url
+        url_data['domain'] = urlparse(url_data['long-url']).netloc
+
+        soup = BeautifulSoup(response)
+        url_data['title'] = soup.title.string
+        url_data['meta-description'] = soup.find("meta", {"name":"description"})['content']
+        url_data['meta-keywords'] = soup.find("meta", {"name":"keywords"})['content']
+
+        return url_data
+
+def main():
+    print expand(url=URL2)
+
+if __name__ == '__main__':
+    main()
